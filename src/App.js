de8ac7d8133d8468650922d059f0f989787df8bb
Chamnanwit <chamnanwiteamdit@gmail.com>
2023-04-24 16:57:23 +0700

    finish-typography

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.scss';
-import {FaHome} from 'react-icons/fa'
+import { FaHome, FaInbox, FaCalendarAlt, FaCalendar, FaChevronDown } from 'react-icons/fa'
 
 function App() {
   return (
@@ -12,7 +12,54 @@ function App() {
       <div>CC-Todoist</div>
       </header>
 
-    <aside className='sidebar'>Sidebar</aside>
+    <aside className='sidebar'>
+      <section className='sidebar__generic'>
+        <ul className='generic__lists'>
+          <li className='active'>
+            <span>
+              <FaInbox />
+            </span>
+            <h6>Inbox</h6>
+          </li>
+          <li>
+            <span>
+              <FaCalendar />
+            </span>
+            <h6>Today</h6>
+          </li>
+          <li>
+            <span>
+              <FaCalendarAlt />
+            </span>
+            <h6>Next 7 days</h6>
+          </li>
+          
+        </ul>
+      </section>
+      <section className='sidebar__middle'>
+        <span>
+          <FaChevronDown />
+        </span>
+        <h6>Projects</h6>
+      </section>
+      <section className='sidebar__projects'>
+      <ul className='generic__lists'>
+          <li className=''>
+            <span>
+              <FaInbox />
+            </span>
+            <h6>Project-A</h6>
+          </li>
+          <li>
+            <span>
+              <FaInbox />
+            </span>
+            <h6>Project-B</h6>
+          </li>
+          
+        </ul>
+      </section>
+    </aside>
 
     <main className='content'>Content</main>
   </div>
