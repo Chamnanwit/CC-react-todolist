740a8b0df94b96ec5bd81a2853d90aa3158f7f0a
Chamnanwit <chamnanwiteamdit@gmail.com>
2023-04-25 10:12:44 +0700

    finished:UI todo-contents

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,4 +1,5 @@
 import styles from './TodoContent.module.scss'
+import { HiCheck , HiPencil , HiTrash } from 'react-icons/fa'
 
 export function TodoContent () {
     const mockTodo = Array.from({length:50},(el,idx)=> idx+1)
@@ -8,15 +9,49 @@ export function TodoContent () {
     console.log(now.toLocaleDateString('en-US', options));
     return (
         <main className='content'>
+
+      {/* TODO_header */}
       <div className={styles.header}>
         <h2>Inbox</h2>
         <p>{now.toLocaleDateString('en-US', options)}</p>
+
+      {/* ADD_TODO */}
       </div>
-      <div>
-        Add todo
+      <div className={styles.add__todo}>
+        <span>+</span>
+        <h3>Add task</h3>
       </div>
+
+      {/* TODO_from */}
+      <form className={styles.todo__form__container}>
+        <input className={styles.todo__form__input} placeholder='Task Name' />
+        <div className={styles.todo__form__footer}>
+            <p className={styles.todo__error}>Title is required</p>
+            <div className={styles.todo__form__buttons}>
+                <button>Cancel</button>
+                <button>Add Task</button>
+            </div>
+        </div>
+      </form>
+
+      {/* TODO_lists */}
       <ul>
-        {mockTodo.map((el)=><li key={el}>{`item-${el}`}</li>)}
+        {mockTodo.map((el)=>(
+          <li className={styles.todo__item__container} key={el}>
+          <div className={styles.checkbox__container}>
+              <HiCheck className={styles.checkbox__icon} />
+          </div>
+          <p className={styles.done}>{`item-${el}`}</p>
+      
+          <div className={styles.edit__icon}>
+              <HiPencil />
+          </div>
+      
+          <div className={styles.delete__icon}>
+              <HiTrash />
+          </div>
+      </li>
+        ))}
       </ul>
     </main>
     );
