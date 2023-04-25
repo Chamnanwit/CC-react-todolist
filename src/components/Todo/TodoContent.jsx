260dada787bbbaad1f343aabdfeced6bc921ad39
Chamnanwit <chamnanwiteamdit@gmail.com>
2023-04-25 14:48:02 +0700

    update

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,12 +1,8 @@
-
 import { AddTodo } from './AddTodo';
 import { TodoHeader } from './TodoHeader';
-import { TodoLists } from './TodoLists'
-
+import { TodoLists } from './TodoLists';
 
 export function TodoContent() {
-   
-
     return (
         <main className='content'>
             <TodoHeader />
--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,17 +1,65 @@
-import styles from './TodoForm.module.scss'
+import styles from './TodoForm.module.scss';
+import React, { useState } from 'react';
 
-export function TodoForm () {
+// TodoForm => call in 2 Mode
+// Mode-1 : Add 
+// Mode-2 : Edit
+export function TodoForm({onSetIsShowForm,submitText, oldTask}) {
+    // # 1 : Logic Section
+    const [task, setTask] = useState(oldTask || '');
+    const [isError,setIsError] = useState(false)
 
-	return (
-		<form className={styles.todo__form__container}>
-		<input className={styles.todo__form__input} placeholder='Task Name' />
-		<div className={styles.todo__form__footer}>
-			<p className={styles.todo__error}>Title is required</p>
-			<div className={styles.todo__form__buttons}>
-				<button>Cancel</button>
-				<button>Add Task</button>
-			</div>
-		</div>
-	</form>
-	)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // validate
+        if(task.trim() === '') {
+            setIsError(true)
+            return;
+        }
+
+        // set back to normal mode
+		onSetIsShowForm(false)
+	
+    };
+    const handleClickCancel = (e) => {
+        // from <AddTodo/> for Add
+		// onSetIsAddMode?.(false)
+        // from <TodoItem/> for Edit
+        // onSetIsEditMode?.(false)
+
+        // for Add , for Edit
+        onSetIsShowForm?.(false)
+	
+
+    };
+
+    const handleChangeInput = (e) => {
+        setIsError(false)
+		setTask(e.target.value)
+    };
+
+ 
+
+    // # 2 : UI-Section
+    return (
+        <form className={styles.todo__form__container} onSubmit={handleSubmit}>
+            <input
+                className={styles.todo__form__input}
+                placeholder='Task Name'
+                value={task}
+                onChange={handleChangeInput}
+            />
+            <div className={styles.todo__form__footer}>
+               {isError &&  <p className={styles.todo__error}>Task Name is required</p>}
+                <div className={styles.todo__form__buttons}>
+                    <button type='button' onClick={handleClickCancel}>
+                        Cancel
+                    </button>
+                    <button type='submit'>{submitText}</button>
+                </div>
+            </div>
+
+        </form>
+    );
 }
