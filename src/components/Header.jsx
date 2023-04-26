d9f16101a6978b77b0d72d0caabe6282b30f6288
Chamnanwit <chamnanwiteamdit@gmail.com>
2023-04-26 16:37:44 +0700

    semi final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,34 @@
-import { FaHome } from 'react-icons/fa';
-import {getFormattedDate} from '../../src/utils/DateUtils'
+import {useState} from 'react'
+import { FaHome, FaSearch } from 'react-icons/fa';
 
-export function Header () {
+export function Header({onSearchText}) {
+
+    const [searchValue,setSearchValue] = useState('')
+
+    const handleChangeText = (e) => {
+        setSearchValue(e.target.value)
+        onSearchText(e.target.value)
+    }
     return (
         <header className='header'>
             <span>
                 <FaHome />
             </span>
-        <div>CC-Todoist by MooJobs.</div>
-      </header>
-    )
+            <h3>CC-Todoist-Moojobs</h3>
+
+            {/* SearchBar */}
+            <div className='header__search__container'>
+                <span className='header__search__icon'>
+                    <FaSearch />
+                </span>
+                <input 
+                type='text' 
+                className='header__search__input' 
+                placeholder='search' 
+                value={searchValue}
+                onChange={handleChangeText}
+                />
+            </div>
+        </header>
+    );
 }
