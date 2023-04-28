6b779835f874ad634397ec42e14dbc64979f49e2
Chamnanwit <chamnanwiteamdit@gmail.com>
2023-04-28 17:00:24 +0700

    update todo

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,22 +1,35 @@
 import styles from './TodoItem.module.scss';
 import { useState } from 'react';
+import axios from 'axios';
 import { HiCheck, HiPencil, HiTrash } from 'react-icons/hi';
 import { TodoForm } from './TodoForm';
 import { getFormattedDate } from '../../utils/DateUtils';
+import PropTypes from 'prop-types';
+
+TodoItem.propTypes =  { 
+    onAddTodo : PropTypes.func,
+    onEditTodo : PropTypes.func,
+    todo :  PropTypes.oneOfType([PropTypes.object])
+}
 
 //todoSchema :  {id:1, task: asdadsasdas, status : false, due_date : 2002-04-20}
 export function TodoItem({ todo, onEditTodo, onDeleteTodo }) {
-    //prop = { todo:{id:1 task : "AA"}, onEditTodo: fn1, onDeleteTodo: fn2 }
-    // let todo = prop.todo
-    // let onEditTodo = prop.onEditTodo
-    // #1 : Logic,State
-    // Check === DONE === todo.status == true
+
     const [isEdit, setIsEdit] = useState(false);
     // console.log(todo.id)
 
-    const handleToggleCheck = () => {
-        // setIsCheck(!isCheck);
-        onEditTodo(todo.id, { status: !todo.status }); // handleEditTodo(todo.id, {status:!todo.status})
+    const updateTodoStatus = async () => {
+       
+        try {
+            let updateTodo = {...todo,status: !todo.status}
+            let response = await axios.put(`http://localhost:8080/todos/${todo.id}`,updateTodo)
+            let updatedTodo = response.data.todo
+            onEditTodo(updatedTodo.id, {status : updatedTodo.status})
+        } catch (error) {
+            console.log(error.response.status)
+        }
+     
+        // onEditTodo(todo.id, { status: !todo.status }); // handleEditTodo(todo.id, {status:!todo.status})
     };
 
     const handleOpenEditMode = () => {
@@ -25,9 +38,9 @@ export function TodoItem({ todo, onEditTodo, onDeleteTodo }) {
     };
 
     const handleDeleteTodo = () => {
-        console.log('delete');
+      
         onDeleteTodo(todo.id);
-        // setTodos(currentTodos => currentTodos.filter(todoObj=> todoObj.id !== todo.id))
+
     };
 
     let checkboxStyle = todo.status ? styles.checkbox__icon__done : styles.checkbox__icon;
@@ -37,11 +50,13 @@ export function TodoItem({ todo, onEditTodo, onDeleteTodo }) {
         <>
             {!isEdit ? (
                 <li className={styles.todo__item__container}>
-                    <div className={styles.checkbox__container} onClick={handleToggleCheck}>
+                    <div className={styles.checkbox__container} onClick={updateTodoStatus}>
                         <HiCheck className={checkboxStyle} />
                     </div>
+
                     <p className={taskStyle}>{todo.task}</p>
                     <span className={styles.date__text}>{getFormattedDate(todo.date)}</span>
+
 
                     <div className={styles.edit__icon} onClick={handleOpenEditMode}>
                         <HiPencil />
